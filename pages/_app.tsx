1d59d64e7757c74ae1a05e94c56d2c8cd5de7780
Marco Pollacci <pollacci@gmail.com>
2022-11-10 16:03:19 +0100

    chore(website): :hammer: settings vscode

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,29 +1,15 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import Script from 'next/script'
-import Head from 'next/head'
+import type { AppProps } from "next/app";
+import Head from "next/head";
+import "../styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-  <> 
-  <Head>
-    <title>Marco Pollacci</title>
-    <meta name="description" content="Marco Pollacci's personal website" />   
-  </Head>
-    {/* <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=UA-21222420-1`} />
-
-      <Script id='lazyG' strategy="lazyOnload">
-          {`
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-              page_path: window.location.pathname,
-              });
-          `}
-      </Script> */}
-    <Component {...pageProps} />
+    <>
+      <Head>
+        <title>Marco Pollacci</title>
+        <meta name="description" content="Marco Pollacci's personal website" />
+      </Head>
+      <Component {...pageProps} />
     </>
-  )
+  );
 }
-

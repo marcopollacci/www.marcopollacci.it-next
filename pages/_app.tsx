fb53d9ab94ab3a8eb86d22a2f5c9450ecbd40baf
Marco Pollacci <pollacci@gmail.com>
2022-11-10 13:22:14 +0100

    feat: :tada: first commit

---
+++ pages/_app.tsx
@@ -0,0 +1,24 @@
+import '../styles/globals.css'
+import type { AppProps } from 'next/app'
+import Script from 'next/script'
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+  <> 
+    {/* <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=UA-21222420-1`} />
+
+      <Script id='lazyG' strategy="lazyOnload">
+          {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+              });
+          `}
+      </Script> */}
+    <Component {...pageProps} />
+    </>
+  )
+}
+

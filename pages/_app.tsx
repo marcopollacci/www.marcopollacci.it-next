c3f55076ffe0c4f0755d02649bd0044c1e2d04ae
Marco Pollacci <pollacci@gmail.com>
2022-11-19 12:03:30 +0100

    style(website): :lipstick: refactor css style

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import { Analytics } from "@vercel/analytics/react";
 import type { AppProps } from "next/app";
 import Head from "next/head";
-import "../styles/globals.css";
+import "../styles/globals.scss";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
